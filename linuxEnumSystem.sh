69452a089b461091ada177d99e6a73e383922afb
guimaraesgu <guguima@gmail.com>
2021-07-09 15:23:03 -0400

    Cleaning System Info

--- linuxEnumSystem.sh
+++ linuxEnumSystem.sh
@@ -7,13 +7,14 @@ echo ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 echo ""
 echo ".:: SYSTEM INFO ::."
 uname -a
-cat /etc/*-release
 cat /etc/issue
 
+echo ""
 echo ".:: NETWORK INFO ::."
 route 
 netstat -nlpt
 
+echo ""
 echo ".:: PRIVILEGE ESCALATION POSSIBILITIES ::."
 echo ""
 echo "--> SUID PRIVILEGE ESCALATION"
@@ -22,11 +23,11 @@ find / -perm -u=s -type f 2>/dev/null
 echo ""
 echo "--> SGID PRIVILEGE ESCALATION"
 echo ""
-find / -perm +2000 -type f 2>/dev/null 
+find / -perm -2000 -type f 2>/dev/null 
 echo ""
 echo "--> STICKY BITS PRIVILEGE ESCALATION"
 echo ""
-find / -perm +1000 -type f 2>/dev/null 
+find / -perm -1000 -type f 2>/dev/null 
 echo ""
 echo "--> CAPABILITIES PRIVILEGE ESCALATION"
 echo ""
@@ -34,5 +35,7 @@ getcap -r / 2>/dev/null
 echo ""
 echo "--> CRONTAB PRIVILEGE ESCALATION"
 echo ""
-ls -la /etc/cron*
+ls -la /etc/crontab
+cat /etc/crontab
+ls -la /etc/cron.*
 
